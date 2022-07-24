ab8d5ff8c5897d1c06e6407383aed14d905c3841
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-07-24 05:49:16 +0300

    Done.

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question ,onDelete }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,6 +9,19 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function deleteQuestion() {
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: 'DELETE',
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(r => r.json())
+      .then(() => onDelete(question))
+  }
+
+
+  
   return (
     <li>
       <h4>Question {id}</h4>
@@ -17,7 +30,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={deleteQuestion}>Delete Question</button>
     </li>
   );
 }
