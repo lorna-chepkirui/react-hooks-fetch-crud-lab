119dc298518442b885bb3efc7059c1fb2f0023ab
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-07-24 05:49:10 +0300

    commit tree 28fda552c7c7a2012f367d96f34333f179b98801

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,20 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [question, setQuestions]= useState([]);
 
+  const setNewQuestion = (newQuestion) => {
+    setQuestions([...question, newQuestion])
+  }
+
+const onDelete= (data) => {
+  const getQuestion = question.filter(question => question.id !== data.id)
+  setQuestions(getQuestion)
+};
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm setNewQuestion={setNewQuestion} /> : <QuestionList onDelete={onDelete}/>}
     </main>
   );
 }
